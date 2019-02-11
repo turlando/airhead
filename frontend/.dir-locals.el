77b1da6fd7dbf3404f1696080daf658508df919e
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-02-11 18:27:02 +0100

    Fix cider integration

--- frontend/.dir-locals.el
+++ frontend/.dir-locals.el
@@ -2,12 +2,7 @@
                               tabs tab-mark indentation::tab
                               spaces space-mark indentation::space
                               newline newline-mark
-                              trailing))
-
-         (clojure-align-forms-automatically . t)
-         (cider-lein-parameters . "with-profile +dev repl")
-
-         (eval . (setq cider-cljs-lein-repl
-                       "(do (require 'figwheel-sidecar.repl-api)
-                            (figwheel-sidecar.repl-api/start-figwheel!)
-                            (figwheel-sidecar.repl-api/cljs-repl))"))))
+                              trailing))))
+ (clojurescript-mode . ((projectile-project-type . lein-test)
+                        (clojure-align-forms-automatically . t)
+                        (cider-lein-parameters . "with-profile +dev repl")))
--- frontend/project.clj
+++ frontend/project.clj
@@ -6,22 +6,28 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.8.0"
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-less "1.7.5"]]
+  :plugins [[lein-doo "0.1.10"]
+            [lein-cljsbuild "1.1.7"]
+            [deraen/lein-less4j "0.6.2"]]
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/clojurescript "1.10.439"
+                  :exclusions [com.google.errorprone/error_prone_annotations
+                               com.google.code.findbugs/jsr305
+                               com.fasterxml.jackson.core/jackson-core]]
                  [reagent "0.8.1"]
                  [cljs-http "0.1.43"]
                  [jarohen/chord "0.8.1"]
                  [markdown-clj "0.9.99"]]
 
-
-  :clean-targets ^{:protect false} ["resources/public/js"
+  :clean-targets ^{:protect false} ["target"
+                                    "node_modules"
+                                    "package.json"
+                                    "package-lock.json"
+                                    "resources/public/js"
                                     "resources/public/css"
-                                    "target"
-                                    ".nrepl-port"
                                     "figwheel_server.log"
+                                    ".nrepl-port"
                                     ".lein-repl-history"
                                     ".rebel_readline_history"]
 
@@ -37,11 +43,7 @@
                     :asset-path           "js/out"
                     :source-map-timestamp true
                     :preloads             [devtools.preload]
-                    :external-config      {:devtools/config
-                                           {:features-to-install    [:formatters :hints]
-                                            :fn-symbol              "F"
-                                            :print-config-overrides true}}}
-     ;; figwheel client config
+                    :external-config      {:devtools/config {:features-to-install :all}}}
      :figwheel     {:websocket-url "ws://[[server-hostname]]:[[server-port]]/figwheel-ws"
                     :on-jsload     "airhead-frontend.core/mount-root"}}
 
@@ -49,34 +51,37 @@
      :source-paths ["src/cljs"]
      :compiler     {:main            airhead-frontend.core
                     :closure-defines {goog.DEBUG false}
-                    :optimizations   :advanced
                     :output-to       "resources/public/js/app.js"
-                    :output-dir      "resources/public/js/min"
+                    :optimizations   :advanced
                     :elide-asserts   true
                     :pretty-print    false}}]}
 
-  :less {:source-paths ["src/less"]
+  :less {:source-paths ["src/less" "node_modules"]
          :target-path  "resources/public/css"}
 
   :profiles
-  {:dev
-   {:plugins      [[lein-figwheel "0.5.17"]]
-    :dependencies [[figwheel-sidecar "0.5.17"]
-                   [cider/piggieback "0.3.10"]
-                   [binaryage/devtools "0.9.9"]]
-    :source-paths ["src/clj"]
-    :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
-    ;; figwheel server config
-    :figwheel     {:server-port 8081
-                   :css-dirs    ["resources/public/css"]}}}
+  {:dev {:plugins        [[lein-figwheel "0.5.17"]]
+         :dependencies   [[figwheel-sidecar "0.5.17"
+                           :exclusions [[org.clojure/tools.nrepl]
+                                        [args4j]]]
+                          [cider/piggieback "0.3.10"
+                           :exclusions [[org.clojure/tools.logging]
+                                        [args4j]]]
+                          [binaryage/devtools "0.9.10"]]
+         :source-paths   ["src/clj/dev"]
+         :resource-paths ["resources" "node_modules"]
+         :repl-options   {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+         :less           {:source-map true}
+         :figwheel       {:server-port 8081
+                          :css-dirs    ["resources/public/css"]}}}
 
   :aliases
   {"dev"   ["with-profile" "+dev" "do"
             ["clean"]
             ["cljsbuild" "once" "dev"]
-            ["less" "once"]
+            ["less4j" "once"]
             ["figwheel" "dev"]]
    "build" ["with-profile" "-dev" "do"
             ["clean"]
-            ["less" "once"]
+            ["less4j" "once"]
             ["cljsbuild" "once" "min"]]})
